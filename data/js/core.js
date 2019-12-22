23f40a14a7cd4585de0708fd58d402c6de6ec68f
sasha <ku3menko.sasha@gmail.com>
2019-12-22 19:38:43 +0100

    responsive navbar added

--- data/js/core.js
+++ data/js/core.js
@@ -7,4 +7,27 @@ document.querySelectorAll('a[href^="#"]').forEach(element => {
     });
 });
 
+const body = document.querySelector('body');
+const menuBtn = document.querySelector('.menu-btn');
+const navLi = document.querySelectorAll('.nav-li');
+const logo = document.querySelector('.logo');
+
+body.addEventListener('click', handleEvent);
+
+function toggleNav() { 
+    Array.from(navLi).forEach(el => {
+        el.classList.toggle('nav-li-opened');
+    });
+    logo.classList.toggle('logo-menu-opened');  
+    menuBtn.classList.toggle('menu-btn-active');
+}
+
+function handleEvent(e) {
+    const target = e.target;
+    if (!target.closest('nav') && menuBtn.classList.contains('menu-btn-active')) {
+        toggleNav();   
+    }  else if (target.classList.contains('menu-btn')) {
+        toggleNav();
+    }  
+}
 
