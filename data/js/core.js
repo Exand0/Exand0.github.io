e54eaaacdd31ec433dca695c0d53c4cfb9906871
sasha <ku3menko.sasha@gmail.com>
2019-12-22 20:39:35 +0100

    refactoring

--- data/js/core.js
+++ data/js/core.js
@@ -1,18 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(element => {
-    element.addEventListener('click', e => {
-        e.preventDefault();
-        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 const body = document.querySelector('body');
 const menuBtn = document.querySelector('.menu-btn');
 const navLi = document.querySelectorAll('.nav-li');
 const logo = document.querySelector('.logo');
-
-body.addEventListener('click', handleEvent);
 
 function toggleNav() { 
     Array.from(navLi).forEach(el => {
@@ -22,12 +11,37 @@ function toggleNav() {
     menuBtn.classList.toggle('menu-btn-active');
 }
 
+
+/* document.querySelectorAll('a[href^="#"]').forEach(element => {
+    element.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}); */
+
+function smoothScroll(element) {
+
+}
+
 function handleEvent(e) {
     const target = e.target;
     if (!target.closest('nav') && menuBtn.classList.contains('menu-btn-active')) {
         toggleNav();   
+
     }  else if (target.classList.contains('menu-btn')) {
         toggleNav();
-    }  
+
+    } else if (target.nodeName === 'A' ) {
+        if (target.getAttribute('href').charAt(0) === '#') {
+            e.preventDefault();
+            document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }
 }
+
+body.addEventListener('click', handleEvent);
 
